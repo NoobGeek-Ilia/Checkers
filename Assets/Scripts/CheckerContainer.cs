77e982dacd4cb22b6833aa67f8503360474be995
NoobGeek-Ilya <popovilya19966@gmail.com>
2024-03-05 14:59:49 +0300

    first

--- Assets/Scripts/CheckerContainer.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CheckerContainer : MonoBehaviour
-{
-    public List<Transform> FirstPlayerCheckers = new ();
-    public List<Transform> SecondPlayerCheckers = new ();
-}
--- Assets/Scripts/ISelectable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-interface ISelectable
-{
-    void SetHighlight(Color color);
-    void ResetHighlight(Color color);
-}
